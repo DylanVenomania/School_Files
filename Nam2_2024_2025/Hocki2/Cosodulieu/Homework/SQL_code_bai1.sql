765a3ba719119c8f159493859e20cd569f583b0f
DylanVenomania <hoangcamton@gmail.com>
2025-06-07 00:54:48 +0700

    Thêm môn Cơ sở dữ liệu

---
+++ Nam2_2024_2025/Hocki2/Cosodulieu/Homework/SQL_code_bai1.sql
@@ -0,0 +1,130 @@
+﻿
+--1 Hãy cho biết tên các dự án mà nhân viên có mã ‘NV01’ tham gia
+select distinct Duan.TenDa 
+from PhanCong PC 
+join Duan on PC.MaDa = Duan.MaDa where PC.MaDa = 'NV01';
+
+--2 Tính tổng thời gian tham gia các dự án của mỗi nhân viên
+select NV.MaNV, NV.HonV, NV.Tenlot, NV.TenNV, sum( PC.ThoiGian) as TongThoiGian 
+from NhanVien NV
+left join PhanCong PC on NV.MaNV = PC.MaNV 
+group by NV.MaNV, NV.HonV, NV.Tenlot, NV.TenNV;
+
+--3 Cho biết họ tên các nhân viên chưa tham gia dự án nào
+select NV.HonV, NV.Tenlot, NV.TenNV 
+from NhanVien NV 
+left join PhanCong PC on NV.MaNV = PC.MaNV 
+where PC.MaNV is null;
+
+--a Tìm ngày sinh và địa chỉ của nhân viên “Nguyễn Bảo Hùng”
+select NgSinh, Dchi from NhanVien 
+where HonV = 'Nguyen' and Tenlot = 'Bao' and TenNV = 'Hung';
+
+--b Tìm tên và địa chỉ của các nhân viên làm việc cho phòng “Nghiên cứu”
+select NV.HonV, NV.Tenlot, NV.TenNV, NV.Dchi
+from NhanVien NV 
+join PhongBan PB on NV.Phong = PB.MaPB 
+where PB.TenPB = 'Nghien cuu';
+
+
+--c Với mỗi dự án triển khai ở Gò Vấp, cho biết mã dự án, mã phòng quản lý và họ tên, ngày sinh trưởng phòng
+select Da.MaDa, Da.Phong, NV.HonV, NV.Tenlot, NV.TenNV, NV.NgSinh 
+from Duan Da
+join PhongBan PB on Da.Phong = PB.MaPB
+join NhanVien NV on PB.TrPhong = NV.MaNV
+where Da.DiaDiem = 'Go Vap'
+
+
+--d Với mỗi nhân viên, cho biết họ tên nhân viên và họ tên của người quản lý nhân viên đó
+select NV.HonV, NV.Tenlot, NV.TenNV, 
+NQL.HonV as HonQL, NQL.Tenlot as TenlotNQL, NQL.TenNV as TenNQL
+from NhanVien NV
+left join NhanVien NQL on NV.MaNQL = NQL.MaNV;
+
+--e Cho biết mã nhân viên, họ và tên của các nhân viên của phòng “Nghiên cứu” có mức lương từ 30000 đến 50000
+select NV.MaNV, NV.HonV, NV.Tenlot, NV.TenNV
+from NhanVien NV
+join PhongBan PB on NV.Phong = PB.MaPB
+where PB.TenPB = 'Nghien cuu' and NV.Luong between 30000 and 50000;
+
+--f Cho biết mã nhân viên, họ tên nhân viên và mã dự án, tên dự án của các dự án mà họ tham gia
+select NV.MaNV, NV.HonV, NV.Tenlot, NV.TenNV, Da.MaDa, Da.TenDa
+from NhanVien NV
+join PhanCong PC on NV.MaNV = PC.MaNV
+join Duan Da on PC.MaDa = Da.MaDa;
+
+
+--g  Cho biết mã nhân viên, họ tên của những người không có người quản lý
+select NV.MaNV, NV.HonV, NV.Tenlot, NV.TenNV
+from NhanVien NV
+where NV.MaNQL is null;
+
+--h Cho biết họ tên của các trưởng phòng có thân nhân
+select DISTINCT NV.HonV, NV.Tenlot, NV.TenNV
+from NhanVien NV
+join PhongBan PB on NV.MaNV = PB.TrPhong
+join ThanNhan TN on NV.MaNV = TN.MaNV;
+
+--i Tính tổng lương nhân viên, lương cao nhất, lương thấp nhất và mức lương trung bình
+select sum(Luong) as TongLuong, max(Luong) as LuongCaonhat, 
+min(Luong) as LuongThapNhat, avg(Luong) as LuongTrungBinh
+from NhanVien;
+
+
+--j Cho biết tổng số nhân viên và mức lương trung bình của phòng “Nghiên cứu”
+select count(NV.MaNV) as TongSonhanVien, avg(NV.Luong) as LuongTrungBinh
+from NhanVien NV
+join PhongBan PB on NV.Phong = PB.MaPB
+where PB.TenPB = 'Nghien cuu';
+
+--k Với mỗi phòng, cho biết mã phòng, số lượng nhân viên và mức lương trung bình
+select PB.MaPB, count(NV.MaNV) as SoLuongNhanVien, avg(NV.Luong) as LuongTrungBinh
+from PhongBan PB
+LEFT join NhanVien NV on PB.MaPB = NV.Phong
+group by PB.MaPB;
+
+--l Với mỗi dự án, cho biết mã dự án, tên dự án và tổng số nhân viên tham gia
+select Da.MaDa, Da.TenDa, count(PC.MaNV) as SoLuongNhanVien
+from Duan Da
+LEFT join PhanCong PC on Da.MaDa = PC.MaDa
+group by Da.MaDa, Da.TenDa;
+
+--m Với mỗi dự án có nhiều hơn 2 nhân viên tham gia, cho biết mã dự án, tên dự án và số lượng nhân viên tham gia
+select Da.MaDa, Da.TenDa, count(PC.MaNV) as SoLuongNhanVien
+from Duan Da
+join PhanCong PC on Da.MaDa = PC.MaDa
+group by Da.MaDa, Da.TenDa
+having count(PC.MaNV) > 2;
+
+--n Với mỗi dự án, cho biết mã số dự án, tên dự án và số lượng nhân viên phòng số 5 tham gia
+select Da.MaDa, Da.TenDa, count(PC.MaNV) as SoLuongNhanVien
+from Duan Da
+join PhanCong PC on Da.MaDa = PC.MaDa
+join NhanVien NV on PC.MaNV = NV.MaNV
+where NV.Phong = 5
+group by Da.MaDa, Da.TenDa;
+
+
+--o  Với mỗi phòng có nhiều hơn 2 nhân viên, cho biết mã phòng và số lượng nhân viên có lương lớn hơn 25000
+select NV.Phong, count(NV.MaNV) as SoLuongNhanVien
+from NhanVien NV
+where NV.Luong > 25000
+group by NV.Phong
+having count(NV.MaNV) > 2;
+
+
+--p Với mỗi phòng có mức lương trung bình lớn hơn 30000, cho biết mã phòng, tên phòng, số lượng nhân viên của phòng đó
+select PB.MaPB, PB.TenPB, count(NV.MaNV) as SoLuongNhanVien
+from PhongBan PB
+join NhanVien NV on PB.MaPB = NV.Phong
+group by PB.MaPB, PB.TenPB
+having avg(NV.Luong) > 30000;
+
+
+--q Với mỗi phòng có mức lương trung bình lớn hơn 30000, cho biết mã phòng, tên phòng, số lượng nhân viên nam của phòng đó
+select PB.MaPB, PB.TenPB, count(NV.MaNV) as SoLuongNhanVienNam
+from PhongBan PB
+join NhanVien NV on PB.MaPB = NV.Phong
+where NV.Gioitinh = 'Nam'
+group by PB.MaPB, PB.TenPB
+having avg(NV.Luong) > 30000;
---
+++ Nam2_2024_2025/Hocki2/Cosodulieu/Homework/SQL_code_bai2.sql
@@ -0,0 +1,89 @@
+﻿--a Địa chỉ và số điện thoại của Nhà xuất bản “Addison Wesley”
+select DiaChi, Sdt
+from NXB
+where TenNXB = 'Addison Wesley';
+
+
+--b Mã sách và tựa sách của những cuốn sách xuất bản bởi “Addison Wesley”
+select Masach, Tua
+from DauSach
+join NXB on DauSach.MaNXB = NXB.MaNXB
+where NXB.TenNXB = 'Addison Wesley';
+
+
+--c Mã sách và tựa sách của những cuốn sách có tác giả là “Hemingway”
+select DauSach.Masach, Tua
+from DauSach
+join TacGia on DauSach.Masach = TacGia.Masach
+where TacGia.TenTacGia = 'Hemingway';
+
+
+--d Với mỗi đầu sách, cho biết tựa và số lượng cuốn sách thư viện đang sở hữu
+select DauSach.Tua, count(CuonSach.MaCuon) as SoLuongCuon
+from DauSach
+left join CuonSach on DauSach.Masach = CuonSach.Masach
+group by DauSach.Tua;
+
+
+--e Với mỗi độc giả, cho biết tên, địa chỉ và số lượng cuốn sách đã mượn
+select DocGia.TenDG, DocGia.DiaChi, count(Muon.MaCuon) as SoLuongMuon
+from DocGia
+left join Muon on DocGia.MaDG = Muon.MaDG
+group by DocGia.TenDG, DocGia.DiaChi;
+
+
+--f Mã cuốn, tựa sách và vị trí của những cuốn sách do “Addison Wesley” xuất bản
+select CuonSach.MaCuon, DauSach.Tua, CuonSach.ViTri
+from CuonSach
+join DauSach on CuonSach.Masach = DauSach.Masach
+join NXB on DauSach.MaNXB = NXB.MaNXB
+where NXB.TenNXB = 'Addison Wesley';
+
+
+--g Với mỗi đầu sách, cho biết tên nhà xuất bản và số lượng tác giả
+select NXB.TenNXB, DauSach.Tua, count(TacGia.TenTacGia) as SoLuongTacGia
+from DauSach
+join NXB on DauSach.MaNXB = NXB.MaNXB
+left join TacGia on DauSach.Masach = TacGia.Masach
+group by NXB.TenNXB, DauSach.Tua;
+
+
+--h Thông tin độc giả đã mượn từ 5 cuốn sách trở lên
+select DocGia.TenDG, DocGia.DiaChi, DocGia.Sdt
+from DocGia
+join Muon on DocGia.MaDG = Muon.MaDG
+group by DocGia.TenDG, DocGia.DiaChi, DocGia.Sdt
+having count(Muon.MaCuon) >= 5;
+
+
+--i Mã NXB, tên NXB và số lượng đầu sách của NXB trong CSDL
+select NXB.MaNXB, NXB.TenNXB, count(DauSach.Masach) as SoLuongDauSach
+from NXB
+left join DauSach on NXB.MaNXB = DauSach.MaNXB
+group by NXB.MaNXB, NXB.TenNXB;
+
+
+--j  Mã NXB, tên NXB và địa chỉ của những NXB có từ 100 đầu sách trở lên
+select NXB.MaNXB, NXB.TenNXB, NXB.DiaChi
+from NXB
+join DauSach on NXB.MaNXB = DauSach.MaNXB
+group by NXB.MaNXB, NXB.TenNXB, NXB.DiaChi
+having count(DauSach.Masach) >= 100;
+
+
+--k Mã NXB, tên NXB và số lượng tác giả đã hợp tác với NXB đó
+select NXB.MaNXB, NXB.TenNXB, count(distinct TacGia.TenTacGia) as SoLuongTacGia
+from NXB
+join DauSach on NXB.MaNXB = DauSach.MaNXB
+join TacGia on DauSach.Masach = TacGia.Masach
+group by NXB.MaNXB, NXB.TenNXB;
+
+--l l. Tựa sách và số lượng tác giả của những cuốn sách có tác giả là “Hemingway” mà độc giả “Nguyễn Văn A” đã từng mượn
+select DauSach.Tua, count(distinct TacGia.TenTacGia) as SoLuongTacGia
+from DauSach
+join TacGia on DauSach.Masach = TacGia.Masach
+join CuonSach on DauSach.Masach = CuonSach.Masach
+join Muon on CuonSach.MaCuon = Muon.MaCuon
+join DocGia on Muon.MaDG = DocGia.MaDG
+where TacGia.TenTacGia = 'Hemingway' and DocGia.TenDG = 'Nguyễn Văn A'
+group by DauSach.Tua;
